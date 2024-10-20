d2059e0bf6628b48ea53da49847aa851e032b72b
Himanshusaini12 <107533833+Himanshusaini12@users.noreply.github.com>
2024-10-20 13:13:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const cors = require('cors');
+//const cors = require('cors');
 const axios = require('axios');
 const app = express();
 const port = 4000;
@@ -8,7 +8,7 @@ const TELEGRAM_CHAT_ID = '1860470146'; // Replace with your chat ID
 
 // Middleware
 app.use(express.json());
-app.use(cors()); // Enable CORS for all routes
+//app.use(cors()); // Enable CORS for all routes
 
 // Function to send a message to your Telegram bot
 const sendToTelegram = async (message) => {
